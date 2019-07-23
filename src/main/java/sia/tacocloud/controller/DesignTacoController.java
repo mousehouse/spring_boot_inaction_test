8e2c62f7703df3c0832a4c5ebdbcc9faac9b879f
alienware <ckauqnjiao@gmail.com>
2019-07-23 23:20:33 +0800

    for design.html

--- src/main/java/sia/tacocloud/controller/DesignTacoController.java
+++ src/main/java/sia/tacocloud/controller/DesignTacoController.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sia.tacocloud.domain.Ingredient;
 import sia.tacocloud.domain.Ingredient.Type;
+import sia.tacocloud.domain.Taco;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -33,7 +36,25 @@ public class DesignTacoController {
         );
 
         Type[] types = Ingredient.Type.values();
-        return "";
+        for (Type type: types){
+            model.addAttribute(type.toString().toLowerCase(),
+                filterByType(ingredients,type));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
     }
 
+//    @PostMapping
+//    public String processDesign(Design design){
+//        //Save the taco design...
+//        //fix me
+//        log.info("Processing design: " + design);
+//        return "redirect:/orders/current";
+//    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type){
+        return ingredients.stream()
+                        .filter(x -> x.getType().equals(type))
+                        .collect(Collectors.toList());
+    }
 }
