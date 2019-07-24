a67ee7b052c8a8fbdd88ce7a09487fd3ae3d8865
kun.cao <ckquanjiao@outlook.com>
2019-07-24 19:56:43 +0800

    add data persistence

--- src/main/java/sia/tacocloud/domain/Taco.java
+++ src/main/java/sia/tacocloud/domain/Taco.java
@@ -2,10 +2,13 @@ package sia.tacocloud.domain;
 
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+    private Long id;
+    private Date createdAt;
     private String name;
     private List<String> ingredients;
 }
